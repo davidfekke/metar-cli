e165c6c4134a26374c37f048b3d829feb5af585b
davidfekke <david@fekke.com>
2018-02-11 20:58:23 -0500

    Adding TAF to METAR CLI

--- __tests__/fetchMETAR.test.js
+++ __tests__/fetchMETAR.test.js
@@ -1,5 +1,5 @@
 
-const fetchMETAR = require('../lib/fetchmetar');
+const fetchMETAR = require('../lib/fetchavweather').fetchMETAR;
 
 test('Get METAR for Craig field in Jacksonville, Fl', (done) => {
     fetchMETAR('KCRG').then(metar => {
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-const fetchMETAR = require('./lib/fetchmetar');
+const { fetchMETAR, fetchTAF } = require('./lib/fetchavweather');
 const program = require('commander');
 const path = require('path');
 const pkg = require(path.join(__dirname, 'package.json'));
@@ -8,20 +8,38 @@ program
  .version(pkg.version)
  .arguments('[icao]')
  .option('-d, --detail', 'Detailed METAR')
+ .option('-t, --taf', 'Terminal Area Forcasts')
  .parse(process.argv);
 
 if (program.args && program.args[0] && program.args[0].length === 4) {
-    fetchMETAR(program.args[0])
-        .then(json => {
-            if (program.detail) {
-                console.log(json);
-            } else {
-                console.log(json['Raw-Report']);
-            }
-        })
-        .catch(err => {
-            console.error(err);
-        });
+    if (program.taf) {
+        console.log('Requested a TAF');
+        fetchTAF(program.args[0])
+            .then(json => {
+                if (program.detail) {
+                    console.log(json);
+                } else {
+                    console.log(json['Raw-Report']);
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    } else { // is a metar
+        console.log('Requested a METAR');
+        fetchMETAR(program.args[0])
+            .then(json => {
+                if (program.detail) {
+                    console.log(json);
+                } else {
+                    console.log(json['Raw-Report']);
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    
 } else {
     console.log('Airport identifier mus be supplied in 4 character ICAO code');
 }
